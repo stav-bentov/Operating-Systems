af22b493c619e71b885a3476f8137811a04c639c
stav-bentov <stavbentov5@gmail.com>
2022-05-27 17:45:48 +0300

    ex5 upload

---
+++ ex5/pcc_client.c
@@ -0,0 +1,169 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define ARGC 4
+#define BUF_MAX_SIZE 1000000
+#define NUM_BYTES_64INT 8
+
+#define ARGS_ERROR "Client ERROR: incorrect number of command line arguments is passed"
+#define OPEN_FILE_ERROR "Client ERROR: failed opening given file"
+#define SOCKET_ERROR "Client ERROR: failed creating socket"
+#define CONNECT_ERROR "Client ERROR: failed connecting"
+#define WRITE_FILE_TO_SERVER_ERROR "Client ERROR: failed writing file to server"
+#define WRITE_SIZE_TO_SERVER_ERROR "Client ERROR: failed writing file-size to server"
+
+/* Passing a condition of an error in bool- if true it's an error- print it and exit(1)*/
+void error_occured(int bool, char *error_msg);
+
+/* Reads every while loop 1MB- by passing it to buffer and then sents it to server*/
+void write_file_to_server(int sockfd, int file_desc);
+
+/* Given 64 bit number convert and write to socket)*/
+void write_size_to_server(uint64_t file_size, int sockfd);
+
+/* Read C that was sent from server*/
+void read_from_server(int sockfd);
+
+/* given an uint64_t convert it from Big-Endian to Little-Endian (and vice versa) */
+uint64_t endianness_swap(uint64_t val);
+
+int main(int argc, char *argv[])
+{
+    int return_value;
+    int sockfd;
+    uint64_t file_size;
+
+    // ================Set arguments and check================
+    error_occured(argc != ARGC, ARGS_ERROR);
+
+    char *server_IP = argv[1];
+    unsigned int server_port = atoi(argv[2]);
+    char *file_path = argv[3];
+
+    int file_desc = open(file_path, O_RDONLY);
+    error_occured(file_desc == -1, OPEN_FILE_ERROR);
+
+    // ====================Make Connection====================
+    struct sockaddr_in server_address;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    error_occured(sockfd == -1, SOCKET_ERROR);
+
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(server_port); // Note: htons for endiannes
+    server_address.sin_addr.s_addr = inet_addr(server_IP);
+
+    return_value = connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+    error_occured(return_value == -1, CONNECT_ERROR);
+
+    // Got from https://stackoverflow.com/questions/238603/how-can-i-get-a-files-size-in-c
+    struct stat st;
+    stat(file_path, &st);
+    file_size = (uint64_t) st.st_size;
+
+    // Writes (N) to server
+    write_size_to_server(file_size, sockfd);
+
+    // Writes file to server
+    write_file_to_server(sockfd, file_desc);
+
+    // Reads (C) from server
+    read_from_server(sockfd);
+
+    close(file_desc);
+
+    exit(0);
+}
+
+void write_size_to_server(uint64_t file_size, int sockfd)
+{
+    // N- the number of bytes that will be transferred (file_size in network byte order)
+    uint64_t N = (endianness_swap(file_size));
+    int count_int_bytes = NUM_BYTES_64INT;// There are 64/8=8 bytes to send to server
+    int count_unwritten_bytes = NUM_BYTES_64INT;// Number of bytes left to write
+    int count_written_bytes = 0;// Number of bytes written
+    int return_value;
+    
+    while (count_written_bytes < count_int_bytes)
+    {
+        // each byte is starting from (&N+count_sent_bytes)
+        return_value = write(sockfd, &N + count_written_bytes, count_unwritten_bytes);
+        error_occured(return_value == -1, WRITE_SIZE_TO_SERVER_ERROR);
+        count_unwritten_bytes -= return_value;
+        count_written_bytes += return_value;
+    }
+}
+
+void write_file_to_server(int sockfd, int file_desc)
+{
+    char *buffer[BUF_MAX_SIZE];
+    int current_bytes_read;
+    int current_bytes_sent;
+
+    while ((current_bytes_read = read(file_desc, buffer, BUF_MAX_SIZE)) != 0)
+    {
+        current_bytes_sent = write(sockfd, buffer, current_bytes_read);
+        error_occured(current_bytes_sent == -1, WRITE_FILE_TO_SERVER_ERROR);
+    }
+}
+
+void read_from_server(int sockfd)
+{
+    uint64_t result;
+    int retrun_value;
+
+    int count_int_bytes = NUM_BYTES_64INT;// There are 64/8=8 bytes to read from server
+    int count_unread_bytes = NUM_BYTES_64INT;// Number of bytes left to read
+    int count_read_bytes = 0;// Number of bytes read
+    while (count_read_bytes < count_int_bytes)
+    {
+        retrun_value = read(sockfd, &result + count_read_bytes, count_unread_bytes);
+        error_occured(retrun_value == -1, WRITE_SIZE_TO_SERVER_ERROR);
+        count_unread_bytes -= retrun_value;
+        count_read_bytes += retrun_value;
+    }
+
+    uint64_t server_count = endianness_swap(result);
+
+    close(sockfd);
+
+    printf("# of printable characters: %lu\n", server_count);
+}
+
+/*https://stackoverflow.com/questions/105252/how-do-i-convert-between-big-endian-and-little-endian-values-in-c*/
+uint64_t endianness_swap(uint64_t val)
+{
+    uint64_t a, b, c, d, e, f, g;
+    a = (val & 0xFF00000000000000) >> 56;
+    b = (val & 0xFF000000000000) >> 48;
+    c = (val & 0xFF0000000000) >> 40;
+    d = (val & 0xFF00000000) >> 32;
+    e = (val & 0xFF000000) >> 24;
+    f = (val & 0xFF0000) >> 16;
+    g = (val & 0xFF00) >> 8;
+    val = (val & 0x000000FF) << 56;
+
+    val = val + (g << 48) + (f << 40) + (e << 32) + (d << 24) + (c << 16) + (b << 8) + (a);
+    return val;
+}
+
+void error_occured(int bool, char *error_msg)
+{
+    if (bool)
+    {
+        fprintf(stderr, "%s: %s", error_msg, strerror(errno));
+        exit(1);
+    }
+}
---
+++ ex5/pcc_server.c
@@ -0,0 +1,350 @@
+// got help from https://www.geeksforgeeks.org/tcp-server-client-implementation-in-c/
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#define ARGC 2
+#define QUEUE_SIZE 10
+#define UPPER_B_PRINTABLE 126
+#define LOWER_B_PRINTABLE 32
+#define NUM_BYTES_64INT 8
+
+#define ARGS_ERROR "Server ERROR: incorrect number of command line arguments is passed"
+#define SIGACTION_ERROR "Server ERROR: failed sigaction"
+#define SOCKET_ERROR "Server ERROR: failed creating socket"
+#define BIND_ERROR "Server ERROR: failed binding"
+#define LISTEN_ERROR "Server ERROR: failed listening"
+#define ACCEPT_ERROR "Server ERROR: failed accepting"
+#define READ_N_ERROR "Server ERROR: failed reading N from client"
+#define READ_N_TCP_ERROR "Server ERROR: TCP error occured while reading N from client"
+#define READ_K_ERROR "Server ERROR: client process was killed unexpectedly while reading N from client"
+#define READ_FILE_ERROR "Server ERROR: failed reading the file from client"
+#define READ_FILE_TCP_ERROR "Server ERROR: TCP error occured while reading the file from client"
+#define READ_K_FILE_ERROR "Server ERROR: client process was killed unexpectedly while reading the file from client"
+#define WRITE_SIZE_ERROR "Server ERROR: failed writing C to client"
+#define WRITE_K_SIZE_ERROR "Server ERROR: client process was killed unexpectedly while writing C to client"
+#define WRITE_TCP_SIZE_ERROR "Server ERROR: TCP error occured while writing C to client"
+
+uint64_t pcc_total[UPPER_B_PRINTABLE - LOWER_B_PRINTABLE + 1];//Will count how many times each printable characterwas observed in all client connections
+
+int connection_error=0; // TCP errors or unexpected connection close- connection_error=1
+int SIGINT_mood=0;
+int client_procced=0; //0 - no client is being procceed, 1- a client is beeing procceed
+int connfd=-1;// when the server is proccesing a client connfd=accept(), else- connfd=-1 (in case of an error too)
+
+/* passing a condition of an error in bool- if true it's an error- print it and exit(1)*/
+void error_occured_exit(int bool, char *error_msg);
+
+/* passing a condition of an error in bool- if true it's a TCP error or connection termineted- connection_error=1 */
+void error_occured_not_exit(int bool, char *error_msg1,char *error_msg2);
+
+/* given an uint64_t convert it from Big-Endian to Little-Endian (and vice versa) */
+uint64_t endianness_swap(uint64_t val);
+
+/* while the server in “processing a client”- update SIGINT_mood=1 so when this process ends it will be the last, and prints statistics and exit
+else- print statistics and exit */
+void SIGINT_action();
+
+/* A printable character is a byte b whose value is 32 ≤ b ≤ 126*/
+int is_printable(char c);
+
+void print_pcc_total();
+
+int main(int argc, char *argv[])
+{
+    int return_value;
+    int sockfd;
+    uint16_t server_port;
+    int current_count_pc; // Count number of printable characters was observed in one client connection
+
+    // For getting N
+    int count_int_bytes = NUM_BYTES_64INT;
+    int count_unread_bytes; // Number of bytes left to read
+    int count_read_bytes;// Number of bytes read
+    int count_unwritten_bytes;// Number of bytes left to write
+    int count_written_bytes;// Number of bytes written
+    int i;
+
+    // ================Set arguments and check================
+    error_occured_exit(argc != ARGC, ARGS_ERROR);
+    server_port = atoi(argv[1]);
+
+    memset(pcc_total, 0, (UPPER_B_PRINTABLE - LOWER_B_PRINTABLE+1) * sizeof(uint64_t));
+
+    // ===================Set Handle SIGINT===================
+    struct sigaction control_SIGINT;
+    control_SIGINT.sa_handler=&SIGINT_action;
+    control_SIGINT.sa_flags=SA_RESTART;
+    return_value=sigaction(SIGINT,&control_SIGINT,NULL);
+    error_occured_exit(return_value==-1,SIGACTION_ERROR);
+
+    // ===========Listen to incoming TCP connections==========
+    struct sockaddr_in server_address;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    error_occured_exit(sockfd == -1, SOCKET_ERROR);
+
+    //Got from https://stackoverflow.com/questions/24194961/how-do-i-use-setsockoptso-reuseaddr
+    int enableS =1;
+    return_value=setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enableS, sizeof(int));
+    error_occured_exit(return_value == -1, SOCKET_ERROR);
+
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(server_port); 
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);//The socket will be bound to all local interfaces
+
+    return_value = bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+    error_occured_exit(return_value == -1, BIND_ERROR);
+
+    return_value = listen(sockfd, QUEUE_SIZE);
+    error_occured_exit(return_value == -1, LISTEN_ERROR);
+
+
+    // ===================Connection accepted===================
+    while (1)
+    {
+        uint64_t result; // Keeps N as network byte order
+        current_count_pc=0;
+        count_unread_bytes = count_int_bytes;
+        count_read_bytes = 0;
+        count_unwritten_bytes = 8;// Number of bytes left to write
+        count_written_bytes = 0;// Number of bytes written
+        uint64_t N;
+        uint64_t C;
+
+        // ====================================PROCESSING CLIENT====================================
+        connfd = accept(sockfd, NULL, NULL);
+        error_occured_exit(connfd == -1, ACCEPT_ERROR);
+
+        // ====================================Get N from client====================================
+        while (count_read_bytes < count_int_bytes && !connection_error)
+        {
+            return_value = read(connfd, &result + count_read_bytes, count_unread_bytes);
+            if(return_value==0 && count_read_bytes!=count_int_bytes)
+            {// "the client process is killed unexpectedly"
+                fprintf(stderr, "%s: %s", READ_K_ERROR, strerror(errno));
+                connection_error=1;
+            }
+            else
+            {
+                error_occured_not_exit(return_value == -1, READ_N_TCP_ERROR,READ_N_ERROR);
+            }
+
+            // retrun_value is valid -update counters
+            if(!connection_error)
+            {
+                count_unread_bytes -= return_value;
+                count_read_bytes += return_value;
+            }
+        }
+
+        // TCP error or client process was killed unexpectedly
+        if(connection_error)
+        {
+            // No need to handle added data because no data was inserted yet
+            close(connfd);
+            connfd=-1;
+            if(SIGINT_mood)
+            {
+                print_pcc_total();
+            }
+            continue;
+        }
+
+        N = endianness_swap(result);
+
+        // =================================DONE- Get N from client=================================
+
+        // Creates a temporary pcc_total for a current client connection 
+        uint64_t current_pcc_total[UPPER_B_PRINTABLE - LOWER_B_PRINTABLE + 1];
+        memset(current_pcc_total,0,(UPPER_B_PRINTABLE - LOWER_B_PRINTABLE + 1)*sizeof(uint64_t));
+
+        // ==================================Read file from client==================================
+        count_unread_bytes = N;
+        count_read_bytes = 0;
+        while (count_read_bytes < N && !connection_error)
+        {
+            char c; // read every while loop one char
+            int index;
+
+            return_value = read(connfd, &c, sizeof(char));
+            if(return_value==0 && count_read_bytes!=N)
+            {// "the client process is killed unexpectedly"
+                fprintf(stderr, "%s: %s", READ_K_FILE_ERROR, strerror(errno));
+                connection_error=1;
+            }
+            else
+            {
+                error_occured_not_exit(return_value == -1, READ_FILE_TCP_ERROR,READ_FILE_ERROR);
+            }
+
+            // retrun_value is valid
+            if(!connection_error)
+            {
+                // Update counters
+                count_unread_bytes -= return_value;
+                count_read_bytes += return_value;
+
+                // Check if printable and update data
+                if(is_printable(c))
+                {
+                    current_count_pc++;
+                    index=(int)(c)-LOWER_B_PRINTABLE;
+                    current_pcc_total[index]++;
+                }
+
+            }
+        }
+
+        if(connection_error)
+        {
+            close(connfd);
+            connfd=-1;
+            if(SIGINT_mood)
+            {
+                print_pcc_total();
+            }
+            continue;
+        }
+
+        // ============================Write current_count_pc to client=============================
+        C = endianness_swap(current_count_pc);
+        count_unwritten_bytes = count_int_bytes;
+        count_written_bytes = 0;
+        
+        while (count_written_bytes<count_int_bytes && !connection_error)
+        {
+            // each byte is starting from (&N+count_sent_bytes)
+            // TODO check if count_unsent_bytes correct
+            return_value = write(connfd, &C + count_written_bytes, count_unwritten_bytes);
+            
+            if(return_value==0 && count_written_bytes!=count_int_bytes)
+            {// "the client process is killed unexpectedly"
+                fprintf(stderr, "%s: %s", WRITE_K_SIZE_ERROR, strerror(errno));
+                connection_error=1;
+            }
+            else
+            {
+                error_occured_not_exit(return_value == -1, WRITE_TCP_SIZE_ERROR,WRITE_SIZE_ERROR);
+            }
+            
+            if(!connection_error)
+            {
+                count_unwritten_bytes -= return_value;
+                count_written_bytes += return_value;
+            }
+        }
+
+        if(connection_error)
+        {
+            close(connfd);
+            connfd=-1;
+            if(SIGINT_mood)
+            {
+                print_pcc_total();
+            }
+            continue;
+        }
+        
+        // Else- update pcc_total
+        for(i=0;i<(UPPER_B_PRINTABLE - LOWER_B_PRINTABLE + 1);i++)
+        {
+            pcc_total[i]=pcc_total[i]+current_pcc_total[i];
+        }
+
+        if(SIGINT_mood)
+        {
+            close(connfd);
+            connfd=-1;
+            print_pcc_total();
+        }
+
+        close(connfd);
+        connfd=-1;
+    }
+
+    exit(0);
+}
+
+void SIGINT_action()
+{
+    // The server is not processing a client when SIGINT is delivered
+    if(connfd<0)
+    {
+        print_pcc_total();
+        exit(0);
+    }
+    // else- The server is processing a client when SIGINT is delivered
+    SIGINT_mood=1;
+}
+
+void print_pcc_total()
+{
+    int i;
+    for(i=0;i<(UPPER_B_PRINTABLE - LOWER_B_PRINTABLE + 1);i++)
+    {
+        printf("char '%c' : %lu times\n",(i+LOWER_B_PRINTABLE),pcc_total[i]);
+    }
+    exit(0);
+}
+
+int is_printable(char c)
+{ 
+    // (A printable character is a byte b whose value is 32 ≤ b ≤ 126)
+    return (c<=UPPER_B_PRINTABLE && c>=LOWER_B_PRINTABLE);
+}
+
+/*https://stackoverflow.com/questions/105252/how-do-i-convert-between-big-endian-and-little-endian-values-in-c*/
+uint64_t endianness_swap(uint64_t val)
+{
+    uint64_t a, b, c, d, e, f, g;
+    a = (val & 0xFF00000000000000) >> 56;
+    b = (val & 0xFF000000000000) >> 48;
+    c = (val & 0xFF0000000000) >> 40;
+    d = (val & 0xFF00000000) >> 32;
+    e = (val & 0xFF000000) >> 24;
+    f = (val & 0xFF0000) >> 16;
+    g = (val & 0xFF00) >> 8;
+    val = (val & 0x000000FF) << 56;
+
+    val = val + (g << 48) + (f << 40) + (e << 32) + (d << 24) + (c << 16) + (b << 8) + (a);
+    return val;
+}
+
+void error_occured_exit(int bool, char *error_msg)
+{
+    if (bool)
+    {
+        fprintf(stderr, "%s: %s", error_msg, strerror(errno));
+        exit(1);
+    }
+}
+
+void error_occured_not_exit(int bool, char *error_msg1,char *error_msg2)
+{
+    if (bool)
+    { // error occured check which type
+        if(errno==ETIMEDOUT || errno==ECONNRESET || errno== EPIPE)
+        { // TCP errors
+            fprintf(stderr, "%s: %s", error_msg1, strerror(errno));
+            connection_error=1;
+        }
+        else
+        {
+            fprintf(stderr, "%s: %s", error_msg2, strerror(errno));
+            exit(1);
+        }
+    }
+}
